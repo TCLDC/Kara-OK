e02a0a96f2605cea284b773b747ed9e15ff36c13
Vincent Chevalier <info@vincentpchevalier.com>
2017-06-01 15:48:40 -0400

    added bg colors to sections for clarity

--- vendors/scripts/jquery.fullPage.js
+++ vendors/scripts/jquery.fullPage.js
@@ -125,7 +125,7 @@
 
             //scrolling
             css3: true,
-            scrollingSpeed: 700,
+            scrollingSpeed: 1000,
             autoScrolling: true,
             fitToSection: true,
             fitToSectionDelay: 1000,
